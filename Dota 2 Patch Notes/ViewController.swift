d92eb6acd300d579c371ac6b9be6fb799bc5c865
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-08-22 19:57:50 -0700

    Fixed some comments and spacing

--- Dota 2 Patch Notes/ViewController.swift
+++ Dota 2 Patch Notes/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
         title = "Loading API..."
         bodyLabel.text = "Also loading..."
         
-        // Customizing everything
+        // Customizing the UI
         applyCustomizations()
         
         // Get data from API and update app
@@ -29,21 +29,21 @@ class ViewController: UIViewController {
     }
     
     func fetchData() {
-        guard let url = URL(string: "https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=570&count=200&maxlength=10000&format=json") else { return }
-
+        guard let url = URL(string: "https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=570&count=20&maxlength=10000&format=json") else { return }
+        
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let data = data {
                 do {
                     let decoder = JSONDecoder()
                     let appNewsResponse = try decoder.decode(AppNewsResponse.self, from: data)
                     let appNews = appNewsResponse.appnews
-
+                    
                     // Filter news items with titles starting with "Dota 2 Update"
                     let filteredNewsItems = appNews.newsitems.filter { $0.title.hasPrefix("Dota 2 Update") }
-
+                    
                     // This should be changed
                     if filteredNewsItems.count >= 2 {
-                        let newsItem = filteredNewsItems[12]
+                        let newsItem = filteredNewsItems[1]
                         // Remove the unnecessary image text at the beginning of each body
                         let pattern = "\\{STEAM_CLAN_IMAGE\\}/[a-zA-Z0-9]+/[a-zA-Z0-9]+\\.png "
                         let regex = try! NSRegularExpression(pattern: pattern, options: [])
@@ -53,20 +53,20 @@ class ViewController: UIViewController {
                             range: NSRange(location: 0, length: newsItem.contents.utf16.count),
                             withTemplate: ""
                         )
-
+                        
                         // Add a bullet point before each "Fixed"
                         let modifiedContentsWithBullet = modifiedContents.replacingOccurrences(of: "Fixed", with: "• Fixed")
-
+                        
                         // Add two newlines after every line
                         let modifiedContentsWithNewlines = modifiedContentsWithBullet.replacingOccurrences(of: "• Fixed", with: "\n\n• Fixed")
-
+                        
                         // Update UI on the main thread
                         DispatchQueue.main.async {
                             self.bodyLabel.text = modifiedContentsWithNewlines
                             self.title = newsItem.title
                             self.navigationController?.navigationBar.prefersLargeTitles = true
                         }
-
+                        
                         // Print the modified news item details
                         print("Title: \(newsItem.title)")
                         print("Modified Contents: \(modifiedContentsWithNewlines)")
@@ -77,7 +77,7 @@ class ViewController: UIViewController {
             }
         }.resume()
     }
-
+    
     func applyCustomizations() {
         // Customizing large title
         if let titleFont = UIFont(name: "Helvetica-Bold", size: 22) {
@@ -100,6 +100,8 @@ class ViewController: UIViewController {
         scrollView.backgroundColor = .black
         bodyLabel.backgroundColor = .black
         bodyLabel.textColor = .lightGray
+        
+        bodyLabel.isUserInteractionEnabled = true
     }
 }
 
